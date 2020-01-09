45f233a3c36e71d0159a0405e25fdd320ac1dfd5
Antoine.Servolin <antoine.servolin@mines-paristech.fr>
2020-01-09 17:16:17 +0100

    bug fix

--- schemes.py
+++ schemes.py
@@ -20,15 +20,6 @@ def solve_euler_explicit(f, x0, t0, dt, t_tot=2):
 
 	return T, X
 
-def erreur_max(solution, f, x0, dt):
-	T,approx = solve_euler_explicit(f, x0, dt)
-	err_max = 0
-	for t in T:
-		err = solution(t) - approx[t/dt]
-		if abs(err) > err_max :
-			err_max = abs(err)
-	return err_max
-	
 def Runge_Kutta_2(f, x0, t0, dt, t_tot = 2):
 
 	N = ma.floor(t_tot/dt)  # Number of iterations
@@ -47,8 +38,7 @@ def Runge_Kutta_2(f, x0, t0, dt, t_tot = 2):
 
 	return T,X
 	
-def euler_implicite(f, x0, dt, t_tot = 2):
-	
+def euler_implicite(f, x0, t0, dt, t_tot = 2):
 	N = ma.floor(t_tot/dt)  # Number of iterations
 	t,x = t0,x0				# Current time & current position
 
@@ -56,8 +46,8 @@ def euler_implicite(f, x0, dt, t_tot = 2):
 	T = [t]				# Liste de temps
 
 	def phi(approx):
-		return x + f(approx) 
-	for k in range(N):
+		return x + dt*f(approx) 
+	for _ in range(N):
 		t += dt
 		approx_pos = phi(x)
 		x = next_step(approx_pos, phi)
@@ -65,13 +55,13 @@ def euler_implicite(f, x0, dt, t_tot = 2):
 		T.append(t)
 		X.append(x)
 
-	return X,T
+	return T,X
 
 def next_step(approx_pos,phi):
 	for _ in range(100):
 		approx_pos = phi(approx_pos)
 	temp = phi(approx_pos)
-	while (temp-approx_pos)/approx_pos > 0.001 :
+	while abs(temp-approx_pos)/approx_pos > 0.001 :
 		approx_pos = temp
 		temp = phi(approx_pos)
 	return approx_pos
