7a57a73f24e2947dad7c96250efe3674df0f572f
Antoine.Servolin <antoine.servolin@mines-paristech.fr>
2020-01-13 00:50:17 +0100

    ajout des tests IVP et RK4

--- schemes.py
+++ schemes.py
@@ -7,7 +7,6 @@ def solve_euler_explicit(f, x0, t0, dt, t_tot=2):
 	''' Cette fonction renvoie la solution à l'équation différentielle dx/dt = f(x) 
 	avec la méthode d'Euler explicite, pour une durée de t_tot seconde'''
 	
-	#print(f, x0, t0, t_tot, dt)
 	N = int(ma.floor(t_tot/dt))
 	t,x = t0,1*x0    # Donne le temps actuel et la position
 	T = [t]  # Liste de temps
@@ -15,10 +14,10 @@ def solve_euler_explicit(f, x0, t0, dt, t_tot=2):
 	for _ in range(0,N):
 		t += dt    # Nouveau temps
 		x += dt*f(x)  # Nouvelle position
-		T.append(t)
-		X = concatenate((X,x),axis = 1)
+		T = vstack([T,t])
+		X = vstack([X,x])
 
-	return T, X
+	return T, X.T
 
 def Runge_Kutta_2(f, x0, t0, dt, t_tot = 2):
 
@@ -32,10 +31,10 @@ def Runge_Kutta_2(f, x0, t0, dt, t_tot = 2):
 		middle_pos = x + dt/2*f(x)
 		x += dt*f(middle_pos)
 
-		T.append(t)
-		X = concatenate((X,x),axis = 1)
+		T = vstack([T,t])
+		X = vstack([X,x])
 
-	return T,X
+	return T,X.T
 
 def Runge_Kutta_4(f, x0, t0, dt, t_tot = 2):
 
@@ -52,10 +51,10 @@ def Runge_Kutta_4(f, x0, t0, dt, t_tot = 2):
 		middle_pos_4 = f(x + dt*middle_pos_3)
 		x += dt/6*(middle_pos_1 + 2*middle_pos_2 + 2*middle_pos_3 + middle_pos_4)
 
-		T.append(t)
-		X = concatenate((X,x),axis = 1)
+		T = vstack([T,t])
+		X = vstack([X,x])
 
-	return T,X
+	return T,X.T
 
 def euler_implicite(f, x0, t0, dt, t_tot = 2):
 	N = int(ma.floor(t_tot/dt))  # Number of iterations
@@ -69,21 +68,21 @@ def euler_implicite(f, x0, t0, dt, t_tot = 2):
 		t += dt
 		approx_pos = phi(x)
 		x = next_step(approx_pos, phi)
-		T.append(t)
-		X = concatenate((X,x),axis = 1)
+		T = vstack([T,t])
+		X = vstack([X,x])
 
-	return T,X
+	return T,X.T
 
 def next_step(approx_pos,phi):
 	for _ in range(100):
 		approx_pos = phi(1*approx_pos)
 	temp = phi(approx_pos)
-	while linalg.norm(temp-approx_pos)/linalg.norm(approx_pos) > 0.001 :
+	while linalg.norm(temp-approx_pos)/linalg.norm(approx_pos) > 0.1 :
 		approx_pos = 1*temp
 		temp = 1*phi(approx_pos)
 	return approx_pos
 
-def solve_ivp_euler_explicit_variable_step(f, t0, x0, t_f, dtmin = 1e-16, dtmax = 0.01, atol = 1e-6):
+def solve_ivp_euler_explicit_variable_step(f, x0, t0, dtmax, t_f, dtmin = 1e-16, atol = 1e-6):
 	dt = dtmax/10; # initial integration step
 	ts, xs = [t0], [x0]  # storage variables
 	t = t0
@@ -106,3 +105,29 @@ def solve_ivp_euler_explicit_variable_step(f, t0, x0, t_f, dtmin = 1e-16, dtmax 
 		xs = vstack([xs,x_next])
 		t, ti, x = t_next, ti_next, x_next
 		return (ts, xs.T)
+		
+def solve_ivp_euler_explicit_variable_step_test(f, x0, t0, dtmax, t_f, dtmin = 1e-16, atol = 1e-6):
+	dt = dtmax/10; # initial integration step
+	dts, ts, xs = [dt], [t0], [x0]  # storage variables
+	t = t0
+	ti = 0  # internal time keeping track of time since latest storage point : must remain below dtmax
+	x = x0
+	while ts[-1] < t_f:
+		print(ts[-1])
+		while ti < dtmax:
+			t_next, ti_next, x_next = t + dt, ti + dt, x + dt * f(x)
+			x_back = x_next - dt * f(x_next)
+			ratio_abs_error = atol / (linalg.norm(x_back-x)/2)
+			dt = 0.9 * dt * sqrt(ratio_abs_error)
+			if dt < dtmin:
+				raise ValueError("Time step below minimum")
+			elif dt > dtmax/2:
+				dt = dtmax/2
+			t, ti, x = t_next, ti_next, x_next
+		dts = vstack([dts,dt])
+		dt2DT = dtmax - ti # time left to dtmax
+		t_next, ti_next, x_next = t + dt2DT, 0, x + dt2DT * f(x)
+		ts = vstack([ts,t_next])
+		xs = vstack([xs,x_next])
+		t, ti, x = t_next, ti_next, x_next
+	return (dts, ts, xs.T)
--- test_scheme.py
+++ test_scheme.py
@@ -10,6 +10,7 @@ def printf(solveurs, true_f, f, x0, t0, dt, t_tot=2):
 	plt.ylabel("Solution")
 	plt.title(f.__name__ + " simulée sur " + str(t_tot) + " secondes avec un pas de temps de " + str(dt))	
 	for solveur in solveurs:
+		# print(solveur.__name__)
 		T, X = solveur(f,x0,t0,dt,t_tot)
 		F = [true_f(t) for t in T]
 		plt.plot(T, X[0], label = solveur.__name__)
@@ -21,27 +22,28 @@ def erreur_max(solveur, solution, f, x0, t0, dt, t_tot=2):
 	T,approx = solveur(f, x0, t0, dt, t_tot)
 	err_max = 0
 	for t in T:
-		err = linalg.norm(solution(t) - approx[0][int(t/dt)])
-		err = min(err,10---16)
+		err = linalg.norm(solution(t) - approx[0][int((t-t0)/dt)])
+		err = max(err,10^-16)
 		if err > err_max :
 			err_max = err
+	# print(err_max)
 	return err_max
 
-def test(solveurs, solution, f, x0 = array([[1]]), t0 = 0, t_tot = 2):
+def test(solveurs, solution, f, x0 = array([1]), t0 = 0, t_tot = 2):
 	plt.figure()
 	plt.xlabel("-log(pas de temps)")
 	plt.ylim([0,0.4])
 	plt.ylabel("erreur maximale")
 	plt.title("Erreur maximale en fonction du pas de temps choisi pour différents solveurs et la fonction " + f.__name__ + " sur " + str(t_tot) + " secondes")	
 	for solveur in solveurs:
-		print(solveur)
-		dt = 1
+		dt = 0.1
 		Liste_err = []
 		Liste_t = []
-		while dt > 10**-4 :
+		while dt > 10**-2 :
+			print(dt)
 			Liste_err.append(erreur_max(solveur, solution, f, x0, t0, dt, t_tot))
 			Liste_t.append(-ma.log10(dt))
-			dt /= 1.5
+			dt -= 0.5*10**(-3)
 		Lissage(Liste_err)
 		plt.plot(Liste_t, Liste_err, label = solveur.__name__)
 	plt.legend(loc = 0)
@@ -49,8 +51,8 @@ def test(solveurs, solution, f, x0 = array([[1]]), t0 = 0, t_tot = 2):
 
 def Lissage(L):
 	N = len(L)
-	for k in range(20,N):
-		L[k] = min(L[k-19:k+1])
+	for k in range(30,N):
+		L[k] = min(L[k-30:k+1])
 ''' Les fonctions exactes sont doublées de la mention ex'''
 
 def exp(x):
@@ -72,6 +74,24 @@ def cos(xy):
 	x, y = xy
 	return array([y,-x])
 	
+## Tests IVP
+
+def test_ivp(f, f_ex, x0, t0, dt, t_tot):
+	dts,ts,xs = solve_ivp_euler_explicit_variable_step_test(f,x0,t0,dt,t_tot)
+	F = [f_ex(t) for t in ts]
+	plt.figure()
+	plt.subplot(2,1,1)
+	plt.plot(ts,dts)
+	plt.xlabel("temps")
+	plt.ylabel("pas de temps choisi")
+	plt.subplot(2,1,2)
+	plt.plot(ts,xs[0],label='approx')
+	plt.plot(ts,F,label='true')
+	plt.legend(loc=0)
+	plt.xlabel("temps")
+	plt.ylabel("solution")
+	plt.show()
+
 
 # tests 1D
 #printf([euler_implicite], exp_ex, exp, 1, 0, 1) 
@@ -79,6 +99,9 @@ def cos(xy):
 #test([solve_euler_explicit, Runge_Kutta_2, euler_implicite], carre_ex,carre, 1, 0)
 
 #test 2D
-test([solve_euler_explicit, Runge_Kutta_2,Runge_Kutta_4, euler_implicite], cos_ex,cos, array([[1],[0]],dtype = float64), 0)
-#printf([solve_euler_explicit,Runge_Kutta_2,euler_implicite, Runge_Kutta_4],cos_ex, cos, array([[cos_ex(-6.0)],[-ma.sin(-6.0)]]),-6.0,0.01,12)
-#printf([solve_euler_explicit,Runge_Kutta_2,euler_implicite, Runge_Kutta_4],exp_ex, exp, array([[1.0]]),0.0,0.01,12)
+#test([solve_ivp_euler_explicit_variable_step, solve_euler_explicit, Runge_Kutta_2,Runge_Kutta_4], carre_ex,carre, array([1.0]), 1,10)
+#printf([solve_ivp_euler_explicit_variable_step,solve_euler_explicit,euler_implicite,Runge_Kutta_2, Runge_Kutta_4],carre_ex, carre, array([1.0]),1,0.1,10)
+#printf([solve_euler_explicit,Runge_Kutta_2,euler_implicite, Runge_Kutta_4,solve_ivp_euler_explicit_variable_step],carre_ex, carre, array([1.0]),0.0,0.1,12)
+
+#test IVP
+test_ivp(carre,carre_ex,array([1]),0.0,0.01,40)
